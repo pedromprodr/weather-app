96435215ab1dc3e174c4f2433c70200e1c64884c
pedro <pedro@Pedro.myguest.virtualbox.org>
2023-01-20 10:28:23 +0000

    Display the temperature in the today card

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -5,141 +5,165 @@ const overlay = document.querySelector("#overlay");
 
 
 async function changeCity() {
-  let city = document.getElementById("search-field");
-  overlay.style.display = "block";
-  spinner.style.display = "block"; // show the spinner
+    let city = document.getElementById("search-field");
+    overlay.style.display = "block";
+    spinner.style.display = "block"; // show the spinner
 
-  let citydata = await getCity(city.value);
-  try{
-    let cityName = await citydata.name;
-    let countryName = await citydata.country;
-  
-    document.getElementById("city-text").textContent =
-      cityName + ", " + countryName;
-  
-    let textLength = document.getElementById("city-text").textContent.length;
-    let desiredWidth = 0.5;
-    let fontSize = (desiredWidth * 100) / textLength;
-    document.getElementById("city-text").style.fontSize = fontSize + "vw";
-  
-    await refreshTemperatures(cityName);
-    overlay.style.display = "none";
-    spinner.style.display = "none"; 
-  }
-  catch{
-    console.log("ERRO");
-    overlay.style.display = "none";
-    spinner.style.display = "none"; 
-  }
+    let citydata = await getCity(city.value);
+    try {
+        let cityName = await citydata.name;
+        let countryName = await citydata.country;
+        document.getElementById("city-text").textContent =
+            cityName + ", " + countryName;
+
+        let textLength = document.getElementById("city-text").textContent.length;
+        let desiredWidth = 0.5;
+        let fontSize = (desiredWidth * 100) / textLength;
+        document.getElementById("city-text").style.fontSize = fontSize + "vw";
+
+        await refreshTemperatures(cityName);
+
+        overlay.style.display = "none";
+        spinner.style.display = "none";
+    }
+    catch {
+        console.log("ERRO");
+        overlay.style.display = "none";
+        spinner.style.display = "none";
+    }
 
 }
 
-async function refreshTemperatures(cityName){
-    getWeatherForToday(cityName);
+async function refreshTemperatures(cityName) {
+    let data = await getWeatherForToday(cityName);
+    console.log(data);
+    console.log(data.list);
+    let todayData = data.list[0];
+
+    document.getElementById("today-current").innerHTML = parseInt(todayData.main.temp) + "ºC";
+    document.getElementById("today-maxt").innerHTML = parseInt(todayData.main.temp_max) + "ºC";
+    document.getElementById("today-mint").innerHTML = parseInt(todayData.main.temp_min) + "ºC";
+    let image = fetch("http://openweathermap.org/img/wn/"+todayData.weather[0].icon+"@2x.png").then(
+        function (response) {
+            document.getElementById("today-image").setAttribute("src",response.url);
+        }
+    )
+
+
+    for (let i = 1; i < 40; i++) {
+
+    }
+
+
 }
 
 function citySearch() {
-  const cityContainer = document.createElement("div");
-  const contentContainer = document.getElementById("content-container");
-  cityContainer.id = "city-container";
+    const cityContainer = document.createElement("div");
+    const contentContainer = document.getElementById("content-container");
+    cityContainer.id = "city-container";
 
-  const cityText = document.createElement("div");
-  cityText.id = "city-text";
-  cityText.textContent = "Lisbon, PT";
+    const cityText = document.createElement("div");
+    cityText.id = "city-text";
+    cityText.textContent = "Lisbon, PT";
 
-  const citySearchField = document.createElement("input");
-  citySearchField.setAttribute("type", "text");
-  citySearchField.setAttribute("id", "search-field");
-  citySearchField.setAttribute("placeholder", "Search...");
+    const citySearchField = document.createElement("input");
+    citySearchField.setAttribute("type", "text");
+    citySearchField.setAttribute("id", "search-field");
+    citySearchField.setAttribute("placeholder", "Search...");
 
-  const citySearchB = document.createElement("button");
-  citySearchB.setAttribute("id", "search-button");
-  citySearchB.setAttribute("type", "search-button");
-  citySearchB.innerHTML =
-    "<span class='material-symbols-outlined'> search </span>";
-  citySearchB.addEventListener("click", () => {
-    changeCity();
-    document.getElementById("search-field").value = "";
-  });
+    const citySearchB = document.createElement("button");
+    citySearchB.setAttribute("id", "search-button");
+    citySearchB.setAttribute("type", "search-button");
+    citySearchB.innerHTML =
+        "<span class='material-symbols-outlined'> search </span>";
+    citySearchB.addEventListener("click", () => {
+        changeCity();
+        document.getElementById("search-field").value = "";
+    });
 
-  citySearchField.addEventListener("keypress", function (event) {
-    // If the user presses the "Enter" key on the keyboard
-    if (event.key === "Enter") {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      document.getElementById("search-button").click();
+    citySearchField.addEventListener("keypress", function (event) {
 
-    }
-  });
-  cityContainer.appendChild(cityText);
-  cityContainer.appendChild(citySearchField);
-  cityContainer.appendChild(citySearchB);
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("search-button").click();
 
-  contentContainer.appendChild(cityContainer);
+        }
+    });
+    cityContainer.appendChild(cityText);
+    cityContainer.appendChild(citySearchField);
+    cityContainer.appendChild(citySearchB);
+
+    contentContainer.appendChild(cityContainer);
 }
 
 function weatherCards() {
-  const cardContainer = document.createElement("div");
-  cardContainer.id = "card-container";
+    const cardContainer = document.createElement("div");
+    cardContainer.id = "card-container";
 
-  const todayCard = document.createElement("div");
-  todayCard.id = "today-card";
+    const todayCard = document.createElement("div");
+    todayCard.id = "today-card";
 
-  const date = new Date();
-  console.log(date);
-  let p = document.createElement("p");
-  p.id = "today-text";
-  p.textContent = date.getDate() + "/" + date.getMonth() + 1;
-  let infoCont = document.createElement("div");
-  infoCont.id = "today-info-container";
+    const date = new Date();
+    let p = document.createElement("p");
+    p.id = "today-text";
+    p.textContent = date.getDate() + "/" + date.getMonth() + 1;
+    let infoCont = document.createElement("div");
+    infoCont.id = "today-info-container";
 
-  let todayCurr = document.createElement("p");
-  todayCurr.id = "today-current";
-  //REMOVER DEPOIS - APENAS PARA TESTE DE UI
-  todayCurr.textContent = "27'C";
+    const todayImg = document.createElement('img');
+    todayImg.id = "today-image";
 
-  let todayMinMax = document.createElement("div");
-  todayMinMax.id = "today-minmax";
-  
+    let todayCurr = document.createElement("p");
+    todayCurr.id = "today-current";
+    //REMOVER DEPOIS - APENAS PARA TESTE DE UI
+    todayCurr.textContent = "27'C";
 
-  let todayMaxT = document.createElement("p");
-  todayMaxT.id = "today-maxt";
-  //REMOVER DEPOIS - APENAS PARA TESTE DE UI
-  todayMaxT.textContent = "36'C";
+    let todayMinMax = document.createElement("div");
+    todayMinMax.id = "today-minmax";
 
-  let todayMinT = document.createElement("p");
-  todayMinT.id = "today-mint";
-  //REMOVER DEPOIS - APENAS PARA TESTE DE UI
-  todayMinT.textContent = "21'C";
 
-  todayMinMax.appendChild(todayMaxT);
-  todayMinMax.appendChild(todayMinT);
+    let todayMaxT = document.createElement("p");
+    todayMaxT.id = "today-maxt";
+    //REMOVER DEPOIS - APENAS PARA TESTE DE UI
+    todayMaxT.textContent = "36'C";
 
-  infoCont.appendChild(p);
-  infoCont.appendChild(todayCurr);
-  infoCont.appendChild(todayMinMax);
+    let todayMinT = document.createElement("p");
+    todayMinT.id = "today-mint";
+    //REMOVER DEPOIS - APENAS PARA TESTE DE UI
+    todayMinT.textContent = "21'C";
 
-  todayCard.appendChild(infoCont);
+    todayMinMax.appendChild(todayMaxT);
+    todayMinMax.appendChild(todayMinT);
 
-  cardContainer.appendChild(todayCard);
+    infoCont.appendChild(p);
+    infoCont.appendChild(todayCurr);
+    infoCont.appendChild(todayMinMax);
 
-  for (let i = 0; i < 4; i++) {
-    const element = document.createElement("div");
-    element.id = "day" + i + "-card";
-    element.classList.add("day-card");
-    p = document.createElement("p");
-    p.id = "day" + i + "-text";
-    p.classList.add("day-text");
-    console.log(date.getDate);
-    p.textContent = date.getDate() + i + 1 + "/" + date.getMonth() + 1;
+    todayCard.appendChild(infoCont);
+    todayCard.appendChild(todayImg);
 
-    element.appendChild(p);
+    cardContainer.appendChild(todayCard);
 
-    cardContainer.appendChild(element);
-  }
+    for (let i = 0; i < 4; i++) {
+        const element = document.createElement("div");
+        element.id = "day" + i + "-card";
+        element.classList.add("day-card");
+        p = document.createElement("p");
+        p.id = "day" + i + "-text";
+        p.classList.add("day-text");
+        p.textContent = date.getDate() + i + 1 + "/" + date.getMonth() + 1;
 
-  document.getElementById("content-container").appendChild(cardContainer);
+
+        const imgday = document.createElement('img');
+        imgday.id = "day" + i + "-image";
+
+        element.appendChild(p);
+        element.appendChild(imgday);
+
+        cardContainer.appendChild(element);
+    }
+
+    document.getElementById("content-container").appendChild(cardContainer);
 }
 
 export { weatherCards, citySearch };
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -5,12 +5,9 @@ async function getCity(city) {
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
     "&limit=1&appid=1ffdc9ad57e02cc6f9d10ae53a341c18";
-  console.log(city);
   const response = await fetch(url, { mode: "cors" });
 
   const data = await response.json();
-
-  console.log(data);
 
   return data[0];
 }
@@ -22,23 +19,22 @@ async function getWeatherForToday(cityName) {
   const lat = await cityData.lat;
   const lon = await cityData.lon;
 
+
+  
   const url =
     "http://api.openweathermap.org/data/2.5/forecast?lat=" +
     lat +
     "&lon=" +
     lon +
-    "&appid=1ffdc9ad57e02cc6f9d10ae53a341c18";
-  var data = fetch(url, { mode: "cors" })
+    "&units=metric&appid=1ffdc9ad57e02cc6f9d10ae53a341c18";
+  var data = await fetch(url, { mode: "cors" })
     .then(function (response) {
       return response.json();
     })
     .catch(function (err) {
       console.error(err);
     });
-
-  console.log(data[0]);
-
-  return data[0];
+  return data;
 }
 
 export { getWeatherForToday, getCity };
